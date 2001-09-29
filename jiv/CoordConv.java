1ff2a91c8dd58843eea5524ad9b867b2fdc698d0
crisco <crisco>
2001-09-29 02:43:34 +0000

    abandoned development version...

--- jiv/CoordConv.java
+++ jiv/CoordConv.java
@@ -1,5 +1,5 @@
 
-// $Id: CoordConv.java,v 1.1 2001-04-08 00:04:27 cc Exp $
+// $Id: CoordConv.java,v 1.2 2001-09-29 02:43:34 crisco Exp $
 /* 
   This file is part of JIV.  
   Copyright (C) 2000, 2001 Chris A. Cocosco (crisco@bic.mni.mcgill.ca)
@@ -31,9 +31,37 @@ package jiv;
  * change after the initialization.
  *
  * @author Chris Cocosco (crisco@bic.mni.mcgill.ca)
- * @version $Id: CoordConv.java,v 1.1 2001-04-08 00:04:27 cc Exp $ 
+ * @version $Id: CoordConv.java,v 1.2 2001-09-29 02:43:34 crisco Exp $ 
  */
 public final class CoordConv {
+
+    static /*private*/ float start_x=  -90;
+    static /*private*/ float start_y= -126;
+    static /*private*/ float start_z=  -72; 
+    static /*private*/ float step_x=     1;
+    static /*private*/ float step_y=     1;
+    static /*private*/ float step_z=     1;
+
+
+    /** Changes the global (common) voxel-to-world transformation. The
+        default is the one corresponding to the standard
+        "MNI-ICBM-Talairach" (181x217x181) sampling.  
+    */
+    static final public void set( VolumeHeader sampling ) {
+	set( sampling.getStartX(), sampling.getStartY(), sampling.getStartZ(),
+	     sampling.getStepX(), sampling.getStepY(), sampling.getStepZ() );
+    }
+
+    static final public void set( float start_x, float start_y, float start_z, 
+				  float step_x, float step_y, float step_z ) {
+	CoordConv.start_x= start_x;
+	CoordConv.start_y= start_y;
+	CoordConv.start_z= start_z;
+	CoordConv.step_x= step_x;
+	CoordConv.step_y= step_y;
+	CoordConv.step_z= step_z;
+    }
+
 
     /* ** world2voxel ** 
      */
@@ -50,9 +78,9 @@ public final class CoordConv {
 
     static final public void world2voxel( Point3Dint voxel, 
 					  float wx, float wy, float wz) {
-	voxel.x= Math.round( wx + 90F);
-	voxel.y= Math.round( wy + 126F);
-	voxel.z= Math.round( wz + 72F);
+	voxel.x= Math.round( (wx - start_x) / step_x );
+	voxel.y= Math.round( (wy - start_y) / step_y );
+	voxel.z= Math.round( (wz - start_z) / step_z );
 	// Math.round() returns its float argument rounded to the nearest
 	// integral value. If the argument is equidistant from two integers,
 	// the method returns the greater of the two integers. 
@@ -78,9 +106,9 @@ public final class CoordConv {
 
     static final public void voxel2world( Point3Dfloat world, 
 					  int vx, int vy, int vz) {
-	world.x= -90 + vx;
-	world.y= -126 + vy;
-	world.z= -72 + vz;
+	world.x= start_x + step_x * vx;
+	world.y= start_y + step_y * vy;
+	world.z= start_z + step_z * vz;
     }
 
     /** this version can be used to prevent loss of precision (roundup errors),
@@ -97,9 +125,9 @@ public final class CoordConv {
     */
     static final public void voxel2world( Point3Dfloat world, 
 					  float vx, float vy, float vz) {
-	world.x= -90F + vx;
-	world.y= -126F + vy;
-	world.z= -72F + vz;
+	world.x= start_x + step_x * vx;
+	world.y= start_y + step_y * vy;
+	world.z= start_z + step_z * vz;
     }
 
     static final public Point3Dfloat voxel2world( Point3Dint voxel) {
